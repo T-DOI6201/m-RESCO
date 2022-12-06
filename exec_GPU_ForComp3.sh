d49928065afad8c8b1a512d97f6690052604d191
tddoi <kaggle0219doi@gmail.com>
2022-12-06 08:12:44 +0000

    add exec_GPU_Forcomp3.sh

--- exec_GPU_ForComp3.sh
+++ exec_GPU_ForComp3.sh
@@ -5,5 +5,5 @@ export SUMO_HOME=/usr/share/sumo
 for agent in NoActIndex NoApproach NoTotalWait NoQueue NoTotalSpeed
 do
     export GPU_NUMBER=0
-    python3 main.py --agent=${agent} --trials=10 --procs=10 --map=cologne8 --memo=${agent}
+    python3 main.py --agent=${agent} --trials=20 --procs=10 --map=cologne8 --memo=
 done
